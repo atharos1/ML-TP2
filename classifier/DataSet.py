77ba30a8a9ec9fa4f2df0a798a2055a9390e9dc9
Nicolas Barrera <nbarrera@itba.edu.ar>
2020-09-18 07:27:12 -0300

    DataSet, Gain and Entropy functions

--- classifier/DataSet.py
+++ classifier/DataSet.py
@@ -1,8 +1,7 @@
+import copy
 import csv
+import math
 from random import shuffle
-
-from classifier.ClassificationClass import ClassificationClass
-
 
 class DataSet:
     classes: dict
@@ -10,30 +9,77 @@ class DataSet:
     examples: list
     properties: dict
     class_param_index: int
+    props_possible_values: list  # lista de propiedades, contiene diccionarios con frecuencias relativas de cada posible valor
 
-    def __init__(self, examples: list, properties: dict, classes: dict, class_param_index: int):
-        self.examples = examples.copy()
-        self.properties = properties.copy()
-        self.classes = classes.copy()
-        self.classes_relative_frequencies = {}
+    def __init__(self, examples: list, properties: dict, class_param_index: int = None):
+        self.examples = examples
+        self.properties = properties
         self.class_param_index = class_param_index
 
-        for example in self.examples:
-            classes[example[class_param_index]].examples.append(example)
+        self.classes = {}
+        self.classes_relative_frequencies = {}
 
+        if class_param_index is not None:
+            class_list_tmp = {}
+
+            for example in examples:
+                class_list_tmp.setdefault(example[class_param_index], []).append(example)
+
+            for class_name in class_list_tmp.keys():
+                self.classes[class_name] = DataSet(class_list_tmp[class_name], properties)
+
+            for class_name, class_dataset in self.classes.items():
+                assert isinstance(class_dataset, DataSet)  # Para autocompletado del IDE
+
+                # TODO hay que aplicar Laplace?
+                self.classes_relative_frequencies[class_name] = len(class_dataset.examples) / len(
+                    self.examples) if len(self.examples) > 0 else 0
+
+        self.__calculate_props_relative_frequencies()
+
+    def __calculate_props_relative_frequencies(self):
+        self.props_possible_values = []
+        for i in range(len(self.properties)):
+            self.props_possible_values.append({})
+
+        for prop_name, prop_index in self.properties.items():
+            values_dict = self.props_possible_values[prop_index]
+            assert isinstance(values_dict, dict)
+
+            values_dict.clear()
+
+            for example in self.examples:
+                values_dict[example[prop_index]] = values_dict.setdefault(example[prop_index], 0) + 1
+
+            for prop_value, count in values_dict.items():
+                values_dict[prop_value] = (count) / (len(self.examples) )
+
+    def subset(self, attr: str, attr_val: str):
+        example_subset = [ex for ex in self.examples if ex[self.properties[attr]] == attr_val]
+        return DataSet(example_subset, self.properties, self.class_param_index)
+
+    def gain(self, attr: str):
+        acum = self.entropy()
+
+        for attr_val in self.props_possible_values[self.properties[attr]].keys():
+            subset = self.subset(attr, attr_val)
+            acum -= (len(subset.examples) / len(self.examples)) * subset.entropy()
+
+        return acum
+
+    def entropy(self):
+        acum = 0
         for class_name, classification_class in self.classes.items():
-            assert isinstance(classification_class, ClassificationClass)  # Para autocompletado del IDE
+            acum += self.classes_relative_frequencies[class_name] * math.log2(
+                self.classes_relative_frequencies[class_name])
 
-            self.classes_relative_frequencies[class_name] = len(classification_class.examples) / len(
-                self.examples)  # TODO hay que aplicar Laplace?
-            classification_class.calculate_props_relative_frequencies()
+        return -acum
 
     @classmethod
     def build_train_test_set_from_csv(cls, csv_path: str, separator: str, class_param_index: int,
                                       train_set_percentage: int):
         examples = []
         properties = {}
-        classes = {}
 
         with open(csv_path, newline='') as f:
             csv_reader = csv.reader(f, delimiter=separator)
@@ -46,13 +92,12 @@ class DataSet:
                     continue
 
                 examples.append(row)
-                classes.setdefault(row[class_param_index], ClassificationClass(row[class_param_index], properties))
 
         shuffle(examples)
 
         train_set_size = int(train_set_percentage * len(examples))
 
         return (
-            cls(examples[:train_set_size], properties, classes, class_param_index),
-            cls(examples[train_set_size:], properties, classes, class_param_index),
+            cls(examples[:train_set_size], properties, class_param_index),
+            cls(examples[train_set_size:], properties, class_param_index),
         )
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ if __name__ == '__main__':
     train_set: DataSet
     test_set: DataSet
 
-    train_set, test_set = DataSet.build_train_test_set_from_csv("data/titanic.csv", "\t", 1, 0.7)
+    # train_set, test_set = DataSet.build_train_test_set_from_csv("data/titanic.csv", "\t", 1, 0.7)
+    train_set, test_set = DataSet.build_train_test_set_from_csv("data/tennis.csv", ";", 5, 1)
 
     print(train_set)
     print(test_set)
